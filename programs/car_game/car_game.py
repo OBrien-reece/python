cd4050cee0671bb94b81dc45e8f7b1972a30bed2
OBrien-reece <indeche.evans.o@gmail.com>
2023-04-28 13:40:12 +0300

    car game

--- programs/car_game/car_game.py
+++ programs/car_game/car_game.py
@@ -6,20 +6,29 @@
 # any other command will make the program state that it doesn't understand
 
 command = ''
+status = 'idle'
 
-while command[0:] != 'quit':
-    command = input('> ')
-    if command[0:] == 'help':
+while True:
+    command = input('> ').lower()
+    if status == 'started' and command[0:] == 'start':
+        print('The car is already started')
+    elif status == 'stopped' and command[0:] == 'stop':
+        print('The car is already stopped')
+
+    elif command[0:] == 'help':
         print('''
-            start - start the car
-            stop - stop the car
-            quit - quit the program
+                start - start the car
+                stop - stop the car
+                quit - quit the program
         ''')
-    elif command[0:] == 'start':
-          print('> start the car')
-    elif command[0:] == 'stop':
-        print('> stop the car')
+    elif command[0:] == 'start' and status == 'idle':
+          status = 'started'
+          print('> You have started the car')
+    elif command[0:] == 'stop' and status == 'idle':
+        status = 'stopped'
+        print('> You have stopped the car')
     elif command[0:] == 'quit':
+        print('> You have opted to quit the session')
         break
     else:
         print('I do not understand...')
